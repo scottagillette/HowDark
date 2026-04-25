0d7b87761cd4056f2415ef2ac8b73cfdfe8a65b3
scottgillette <scottagillette@gmail.com>
2026-04-25 11:18:43 -0500

    Fix Fire Dragon stat block added.

--- src/main/java/com/redshift/ShadowDarkCalculator/creatures/dragons/FireDragon.java
+++ src/main/java/com/redshift/ShadowDarkCalculator/creatures/dragons/FireDragon.java
@@ -17,7 +17,12 @@ import java.util.List;
 import static com.redshift.ShadowDarkCalculator.dice.SingleDie.*;
 
 /**
- * Fire dragon p.210 core rule book.
+ * Blood-red scales cover the hide of this mighty, volcanic wyrm. Leaping flames glow at the back of its throat.
+ * AC 18, HP 80, ATK 4 rend +11 (2d12) or 1 fire breath
+ * MV double near (fly)
+ * S +6, D +5, C +4, I +4, W +4, Ch +5, AL C, LV 17
+ * Fireblood. Fire immune.
+ * Fire Breath. Fills a double near-sized cube extending from dragon. DC 15 DEX or 6d10 damage.
  */
 
 @Slf4j
