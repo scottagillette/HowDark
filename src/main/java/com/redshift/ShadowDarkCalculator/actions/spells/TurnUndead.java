bcc59e3b43677e3a870a12c202978057e12199a0
scottgillette <scottagillette@gmail.com>
2026-05-04 20:12:50 -0500

    Fix Turn Undead save check and update logging.

--- src/main/java/com/redshift/ShadowDarkCalculator/actions/spells/TurnUndead.java
+++ src/main/java/com/redshift/ShadowDarkCalculator/actions/spells/TurnUndead.java
@@ -15,7 +15,14 @@ import java.util.List;
 import static com.redshift.ShadowDarkCalculator.dice.SingleDie.*;
 
 /**
- * Cause fear in undead and can destroy them if spell check is +10 above save roll.
+ * Tier 1, priest
+ * Duration: Instant
+ * Range: Near
+ *
+ * You rebuke undead creatures, forcing them to flee. You must present a holy symbol to
+ * cast this spell. Undead creatures within near of you must make a CHA check vs. your
+ * spell casting check. If a creature fails by 10+ points and is equal to or less than
+ * your level, it is destroyed. Otherwise, on a fail, it flees from you for 5 rounds.
  */
 
 @Slf4j
@@ -51,24 +58,26 @@ public class TurnUndead extends MultiTargetSpell {
             log.info("{} critically MISSES the spell check on {}", actor.getName(), getName());
         } else if (criticalSuccess || spellCheckRoll + spellCheckModifier + spellCheckBonus >= difficultyClass) {
             targets.forEach(target -> {
+
                 int save = target.getStats().charismaSave();
 
-                if (save < spellCheckRoll + spellCheckModifier) {
+                if (save < spellCheckRoll + spellCheckModifier + spellCheckBonus) {
                     if ((spellCheckRoll + spellCheckModifier) - save >= 10) {
                         if (actor.getLevel() >= target.getLevel()) {
-                            log.info("{} hits a spell on {} with a {} and is destroyed!", actor.getName(), target.getName(), getName());
+                            log.info("{} hits a spell on {} with {} and is destroyed!", actor.getName(), target.getName(), getName());
                             target.takeDamage(999, new DamageType().addMagical()); // Destroyed!
                         } else {
                             target.addCondition(new FearCondition()); // Just feared
-                            log.info("{} hits a spell on {} with a {} and feared!", actor.getName(), target.getName(), getName());
+                            log.info("{} hits a spell on {} with {} and feared!", actor.getName(), target.getName(), getName());
                         }
                     } else {
                         target.addCondition(new FearCondition()); // Just feared
-                        log.info("{} hits a spell on {} with a {} and feared!", actor.getName(), target.getName(), getName());
+                        log.info("{} hits a spell on {} with {} and feared!", actor.getName(), target.getName(), getName());
                     }
                 } else {
-                    log.info("{} has their spell resisted by {} with a {}", actor.getName(), target.getName(), getName());
+                    log.info("{} has their spell {} resisted by {}", actor.getName(), getName(), target.getName());
                 }
+
             });
         } else {
             lost = true; // Failed spell check!
