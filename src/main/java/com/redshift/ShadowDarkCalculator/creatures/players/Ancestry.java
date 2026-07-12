74b419f153d7ec147e294a8f38e1fc1272556a88
scottgillette <scottagillette@gmail.com>
2026-07-11 22:54:23 -0500

    Add TODO

--- src/main/java/com/redshift/ShadowDarkCalculator/creatures/players/Ancestry.java
+++ src/main/java/com/redshift/ShadowDarkCalculator/creatures/players/Ancestry.java
@@ -7,7 +7,7 @@ public enum Ancestry {
     DWARF("Dwarf"), // ADV on hp rolls.
     ELF("Elf"), // +1 to range or spell rolls
     GOBLIN("Goblin"), // No surprise
-    HALF_ELF("Half-Elf"), // Roll 2 on talent roll, choose one
+    HALF_ELF("Half-Elf"), // TODO: Roll 2 on talent roll, choose one
     HALF_ORC ("Half-Orc"), // +1 to melee and damage rolls.
     HALFLING("Halfling"), // Once per day turn invisible for 3 rounds
     HUMAN("Human"), // 2 Talent rolls
